7e189b9ae30463b364412b53cea4e1f234c34f62
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-11-29 16:21:35 +0300

    Update AppendingIterable

--- src/main/java/com/github/nikita_volkov/java/iterables/AppendingIterable.java
+++ src/main/java/com/github/nikita_volkov/java/iterables/AppendingIterable.java
@@ -6,17 +6,17 @@ import java.util.Iterator;
 
 public final class AppendingIterable<a> implements Iterable<a> {
 
-  private final Iterable<a> iterable1;
-  private final Iterable<a> iterable2;
+  private final Iterable<? extends a> iterable1;
+  private final Iterable<? extends a> iterable2;
 
-  public AppendingIterable(Iterable<a> iterable1, Iterable<a> iterable2) {
+  public AppendingIterable(Iterable<? extends a> iterable1, Iterable<? extends a> iterable2) {
     this.iterable1 = iterable1;
     this.iterable2 = iterable2;
   }
 
   @Override
   public Iterator<a> iterator() {
-    return new AppendingIterator<a>(iterable1.iterator(), iterable2.iterator());
+    return new AppendingIterator<>(iterable1.iterator(), iterable2.iterator());
   }
 
 }
